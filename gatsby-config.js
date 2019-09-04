923a60127e6d2738c7c4f24485590b974d6e008a
Moritz Frölich <Moritz.Froelich@tapio.one>
2019-09-04 19:32:34 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: process.env.STORYBLOK_TOKEN,
+        homeSlug: 'home',
+        version: 'draft'
+      }
+    }
+  ]
+}
